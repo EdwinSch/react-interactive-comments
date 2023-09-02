ca9427877162557a5a00f1300f7660be2d862740
front-ed <edwinschutjes@gmail.com>
2023-09-03 00:10:45 +0200

    style: comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,5 +1,26 @@
+import { FaReply } from "react-icons/fa";
+
 const Comment = ({ id, user, date, content, upvotePts, replies }) => {
-  return <article>{content}</article>;
+  return (
+    <article className="single-comment">
+      <div className="upvotes"> + -</div>
+      <div className="comment-wrapper">
+        <div className="comment-header">
+          <div className="user-wrapper">
+            <img src={user.img} alt={user.name} />
+            <p className="user-name">{user.name}</p>
+            <p className="date">{date}</p>
+          </div>
+
+          <button className="reply-btn" type="button">
+            <FaReply />
+            reply
+          </button>
+        </div>
+        <p className="content-text">{content}</p>
+      </div>
+    </article>
+  );
 };
 
 export default Comment;
